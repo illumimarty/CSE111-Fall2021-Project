6cc6d8fc542bb2f50d1e8d74e9e86e28fcefaca4
Marty N <mdrnodado@gmail.com>
2021-11-17 17:53:55 -0800

    started inserting sample data to db

--- populate-tables.sql
+++ populate-tables.sql
@@ -3,5 +3,32 @@
 -- Create Schema
 
 -- Populate Tables
+insert into user values
+    (1, 'mnodado', 'nodado'),
+    (2, 'tchan', 'chan'),
+    (3, 'adarian', 'darian'),
+    (4, 'msingh', 'singh'),
+    (5, 'asiaotong', 'siaotong'),
+    (6, 'mdelosreyes', 'delosreyes'),
+    (7, 'aestrella', 'estrella'),
+    (8, 'aross', 'ross'),
+    (9, 'sduong', 'sduong'),
+    (10, 'flira', 'flira'),;
 
+insert into board values
+    (1, 'Engineering'),
+    (2, 'Design'),
+    (3, 'Directors'),
+    (4, 'Logistics'),
+    (5, 'Marketing');
+
+insert into tasks VALUES
+    ;
+
+insert into boardusers values
+-- bId, uId
+    (1, 2),
+    (1, 3),
+    (1, 6),
+    (1. 8),
 -- Queries
