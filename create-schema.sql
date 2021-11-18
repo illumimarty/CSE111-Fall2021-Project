9a9a996e527cba71e5247087f44541d91088dc67
Marty N <mdrnodado@gmail.com>
2021-11-17 19:04:32 -0800

    added files to separate code

---
+++ create-schema.sql
@@ -0,0 +1,58 @@
+drop table User;
+drop table BoardUsers;
+drop table Board;
+drop table Task;
+drop table TaskAssignee;
+drop table Assignee;
+drop table Comment;
+drop table Image;
+
+-- Create Schema
+CREATE TABLE User (
+    UserId int,
+    Username varchar,
+    Password varchar
+);
+
+CREATE TABLE BoardUsers (
+    UserId int,
+    BoardId int
+);
+
+CREATE TABLE Board (
+    BoardId int,
+    Title string
+);
+
+CREATE TABLE Task (
+    TaskId int,
+    BoardId int,
+    Title string,
+    DueDate varchar,
+    Details string,
+    Image varbinary
+);
+
+CREATE TABLE TaskAssignee (
+    TaskId int,
+    AssigneeId int
+);
+
+CREATE TABLE Assignee (
+    UserId int,
+    Username varchar
+);
+
+CREATE TABLE Comment (
+    TaskId int,
+    UserId int,
+    CommentId int,
+    Message string,
+    Timestamp varchar
+);
+
+CREATE TABLE Image (
+    TaskId int,
+    UserId int,
+    File varbinary
+);
