cf6d47d933690de4daaf06c0f7e6f0976a39a4ee
Truman Chan <tchan38@ucmerced.edu>
2021-11-17 18:35:10 -0800

    Fixed Table Attributes, Populated Task Table, Created DB

--- populate-tables.sql
+++ populate-tables.sql
@@ -4,36 +4,36 @@
 CREATE TABLE User (
     UserId int,
     Username varchar,
-    Password varchar,
+    Password varchar
 );
 
 CREATE TABLE BoardUsers (
     UserId int,
-    BoardId int,
+    BoardId int
 );
 
 CREATE TABLE Board (
     BoardId int,
-    Title string,
+    Title string
 );
 
 CREATE TABLE Task (
     TaskId int,
     BoardId int,
+    Title string,
     DueDate varchar,
     Details string,
-    CommentList string,
-    Image varbinary,
+    Image varbinary
 );
 
 CREATE TABLE TaskAssignee (
     TaskId int,
-    AssigneeId int,
+    AssigneeId int
 );
 
 CREATE TABLE Assignee (
     UserId int,
-    Username varchar,
+    Username varchar
 );
 
 CREATE TABLE Comment (
@@ -41,13 +41,13 @@ CREATE TABLE Comment (
     UserId int,
     CommentId int,
     Message string,
-    Timestamp varchar,
+    Timestamp varchar
 );
 
 CREATE TABLE Image (
     TaskId int,
     UserId int,
-    File varbinary,
+    File varbinary
 );
 
 -- Populate Tables
@@ -61,7 +61,7 @@ insert into user values
     (7, 'aestrella', 'estrella'),
     (8, 'aross', 'ross'),
     (9, 'sduong', 'sduong'),
-    (10, 'flira', 'flira'),;
+    (10, 'flira', 'flira');
 
 insert into board values
     (1, 'Engineering'),
@@ -70,13 +70,17 @@ insert into board values
     (4, 'Logistics'),
     (5, 'Marketing');
 
-insert into tasks VALUES
-    ;
+insert into task VALUES
+    (1, 1, 'New Hover Animation', '11-20-2021', 'Make it so that hovering an image enlarges the image', NULL),
+    (2, 1, 'Fix Text Flexing Into New Line', '1-15-2021', 'Change the width and display property of the text', NULL),
+    (3, 1, 'New Page: Hacktually 2', '11-13-2021', 'Render all the pages onto the MLH Local Hack Learn Day page', NULL),
+    (4, 1, 'Bug: HTML code breaking the website', '10-10-2021', 'URGENT pls someone fix the website aaaa', NULL),
+    (5, 1, 'Fix: Make Sponsors Component responsive!', '12-26-2021', 'At lower resolution widths, the component is too big', NULL);
 
 insert into boardusers values
 -- bId, uId
     (1, 2),
     (1, 3),
     (1, 6),
-    (1. 8),
+    (1. 8)
 -- Queries
